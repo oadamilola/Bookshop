b9b0b14d88a1c9c7f28e70d199a183913835c3a7
oadamilola <145612868+oadamilola@users.noreply.github.com>
2023-10-25 15:48:56 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package bookshop;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Mainframe mframe= new Mainframe();//creates new mainframe
+		mframe.setVisible(true);
+	}
+
+}
